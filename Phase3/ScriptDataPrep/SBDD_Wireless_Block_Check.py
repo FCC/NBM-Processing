7b59d7fdcde7817a4141d9418c267004951712f7
Michael Byrne <michael.byrne@fcc.gov>
2013-05-16 13:47:40 -0300

    fixed logic

--- Phase3/ScriptDataPrep/SBDD_Wireless_Block_Check.py
+++ Phase3/ScriptDataPrep/SBDD_Wireless_Block_Check.py
@@ -13,7 +13,11 @@ from arcpy import env
 import sys, string, os, math
 
 thePGDB = "C:/Users/michael.byrne/Processing.gdb"  #processing file geodatabase
-thePGDB = "C:/Users/Processing.gdb"  #processing file geodatabase
+theLocation = "C:/Users/michael.byrne/NBM/Spring2013/Data/"
+theYear = "2013"
+theMonth = "04"
+theDay = "01"
+
 arcpy.env.workspace = thePGDB
 States = ["AK","AL","AR","AS","AZ","CA","CO","CT"]          #1
 States = States + ["DC","DE","FL","GA","GU","HI","IA","ID"] #2
@@ -22,12 +26,8 @@ States = States + ["MI","MN","MO","MS","MT","NC","ND","MP"] #4
 States = States + ["NE","NH","NJ","NM","NV","NY","OH","OK"] #5
 States = States + ["OR","PA","PR","RI","SC","SD","TN","TX"] #6 
 States = States + ["UT","VA","VI","VT","WA","WI","WV","WY"] #7
+States = ["AK"]
 
-theLocation = "C:/Users/michael.byrne/NBMSource/Fall2011/"
-theLocation = "C:/Users/NBMSource/Fall2011/"
-theYear = "2011"
-theMonth = "10"
-theDay = "01"
 
 ##write out functions
 ##Function sbdd_ExportToShape exports the created layers to shapefiles in
@@ -39,19 +39,19 @@ def checkOverlyRecords():
         rows = arcpy.SearchCursor(theFD + "BB_Service_Wireless")
         for row in rows: #for every row in the delivered wireless layer
             arcpy.AddMessage("     checking ID " + str(myCnt) + " of " + str(theCnt))
-            if arcpy.Exists("wireless_block_overlay_" + theST):  #if the state table exists, get the record count on it
-                myTech = str(row.getValue("TRANSTECH"))
-                myID = row.getValue("SBDD_ID") 
+            myTech = str(row.getValue("TRANSTECH"))
+            myID = row.getValue("SBDD_ID") 
+            if arcpy.Exists("wireless_block_" + theST + "_" + str(myCnt) ):  #if the state table exists, get the record count on it
                 if myTech <> "60":                   
                     myQry = "TRANSTECH <> 60 AND SBDD_ID = '" + str(myID) + "'"
                     myLyr = theST + "SBDD_ID_Check_" + str(myCnt)
-                    arcpy.MakeTableView_management ("wireless_block_overlay_" + theST, myLyr, myQry)
+                    arcpy.MakeTableView_management ("wireless_block_" + theST + "_" + str(myCnt) , myLyr, myQry)
                     if int(arcpy.GetCount_management(myLyr).getOutput(0)) < 1:
                         arcpy.AddMessage("        SBDD_ID " + str(myID) + " is missing from " + theST)
                         del myLyr, myQry
-                else:
-                    arcpy.AddMessage("       this record is a satellite record: " + myID)
                 del myTech, myID
+            elif myTech == "60":
+                arcpy.AddMessage("       this record is a satellite record: " + myID)
             else:
                 arcpy.AddMessage("     the state hasn't been run yet")
             myCnt = myCnt + 1
