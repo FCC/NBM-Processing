c0984fd118b160f6ee211da38c4cb289c2490b1e
Michael Byrne <michael.byrne@fcc.gov>
2013-05-16 14:32:55 -0300

    really updating for spring 2013

--- Phase3/ScriptDataPrep/SBDD_Wireless_Block_Append.py
+++ Phase3/ScriptDataPrep/SBDD_Wireless_Block_Append.py
@@ -1,10 +1,9 @@
 # ---------------------------------------------------------------------------
-# SBDD_Wireless_Block_Check.py
+# SBDD_Wireless_Block_Append.py
 # Created on: May 16, 2011 
 # Created by: Michael Byrne
 # Federal Communications Commission
-# checks to make sure that all SBDD_IDs are accounted for
-# in the overlay output tables that are in the input data
+# Appends the results of the Wireless Block overlay into State Tables
 # ---------------------------------------------------------------------------
 
 # Import system modules
@@ -12,13 +11,11 @@ import arcpy
 from arcpy import env
 import sys, string, os, math
 
-thePGDB = "C:/Users/michael.byrne/Processing_wireless.gdb"  #processing file geodatabase
-theLocation = "C:/Users/michael.byrne/NBM/Spring2013/Data/"
-theYear = "2013"
-theMonth = "04"
-theDay = "01"
+##Global Variables
+thePGDB = "C:/Users/michael.byrne/Processing_wireless.gdb"  #the Output Location
 
 arcpy.env.workspace = thePGDB
+theInFGDB = "C:/Users/michael.byrne/Processing.gdb/" #input file geodatabase
 States = ["AK","AL","AR","AS","AZ","CA","CO","CT"]          #1
 States = States + ["DC","DE","FL","GA","GU","HI","IA","ID"] #2
 States = States + ["IL","IN","KS","KY","LA","MA","MD","ME"] #3 
@@ -29,47 +26,62 @@ States = States + ["UT","VA","VI","VT","WA","WI","WV","WY"] #7
 States = ["AS"]
 
 
+theLocation = "C:/Users/michael.byrne/NBM/Spring2013/Data/"
+theYear = "2013"
+theMonth = "04"
+theDay = "01"
+
 ##write out functions
-##Function sbdd_ExportToShape exports the created layers to shapefiles in
-##appropriate directories
-def checkOverlyRecords():
-    theCnt = int(arcpy.GetCount_management(theFD + "BB_Service_Wireless").getOutput(0))
-    myCnt = 1
-    if theCnt > 0:  #if there are records in the wireless shape class        
-        rows = arcpy.SearchCursor(theFD + "BB_Service_Wireless")
-        for row in rows: #for every row in the delivered wireless layer
-            arcpy.AddMessage("     checking ID " + str(myCnt) + " of " + str(theCnt))
-            if arcpy.Exists("wireless_block_overlay_" + theST):  #if the state table exists, get the record count on it
-                myTech = str(row.getValue("TRANSTECH"))
-                myID = row.getValue("SBDD_ID") 
-                if myTech <> "60":                   
-                    myQry = "TRANSTECH <> 60 AND SBDD_ID = '" + str(myID) + "'"
-                    myLyr = theST + "SBDD_ID_Check_" + str(myCnt)
-                    arcpy.MakeTableView_management ("wireless_block_overlay_" + theST, myLyr, myQry)
-                    if int(arcpy.GetCount_management(myLyr).getOutput(0)) < 1:
-                        arcpy.AddMessage("        SBDD_ID " + str(myID) + " is missing from " + theST)
-                        del myLyr, myQry
-                else:
-                    arcpy.AddMessage("       this record is a satellite record: " + myID)
-                del myTech, myID
-            else:
-                arcpy.AddMessage("     the state hasn't been run yet")
-            myCnt = myCnt + 1
-        del rows, row
-    del  theCnt, myCnt
+##Function sbdd_CreateTable defines a new state table into which all state records will be appended
+def sbdd_CreateTable(myTbl):
+    arcpy.AddMessage("  Creating output table:" + myTbl)
+    if arcpy.Exists(myTbl):
+        arcpy.Delete_management(myTbl)
+    arcpy.CreateTable_management(thePGDB, myTbl)        
+    arcpy.AddField_management(myTbl, "GEOID10", "TEXT", "", "", "15")
+    arcpy.AddField_management(myTbl, "PCT" ,"DOUBLE", "5" , "2", "")
+    arcpy.AddField_management(myTbl, "FRN", "TEXT", "", "", "10")
+    arcpy.AddField_management(myTbl, "PROVNAME", "TEXT", "", "", "200")
+    arcpy.AddField_management(myTbl, "DBANAME", "TEXT", "", "", "200")
+    arcpy.AddField_management(myTbl, "SPECTRUM", "SHORT", "", "", "15")
+    arcpy.AddField_management(myTbl, "TRANSTECH", "SHORT", "", "", "15")
+    arcpy.AddField_management(myTbl, "MAXADDOWN", "TEXT", "", "", "2")
+    arcpy.AddField_management(myTbl, "MAXADUP", "TEXT", "", "", "2")
+    arcpy.AddField_management(myTbl, "TYPICDOWN", "TEXT", "", "", "2")
+    arcpy.AddField_management(myTbl, "TYPICUP", "TEXT", "", "", "2")
+    arcpy.AddField_management(myTbl, "SBDD_ID", "TEXT", "", "", "20")
+    del myTbl
     return ()
+
+
+##Function sbdd_AppendRecords appends records of each set to the state created table
+def sbdd_AppendRecords(myTbl):
+    arcpy.AddMessage("     Begining Append Processing")
+    if arcpy.Exists(theFD + "BB_Service_Wireless"):
+        theCnt = int(arcpy.GetCount_management(theFD + "BB_Service_Wireless").getOutput(0))
+    else:
+        arcpy.AddMessage("     the State has not been processed yet ...")
+        theCnt = 1        
+    myCnt = 1   
+    while myCnt <= theCnt:
+        inTbl = theInFGDB + "wireless_block_" + theST + "_" + str(myCnt)
+        if arcpy.Exists(inTbl):
+            arcpy.AddMessage("     Appending " + str(myCnt) + " of " + str(theCnt))
+            arcpy.Append_management([inTbl], myTbl, "NO_TEST")
+        myCnt = myCnt + 1
+    del myTbl, theCnt, myCnt, inTbl
 
 #****************************************************************************
 ##################Main Code below
 #****************************************************************************
 try:
     for theST in States:
-        theFD = theLocation + theST + "/" + theST + "_SBDD_" + theYear
-        theFD = theFD + "_" + theMonth + "_" + theDay
-        theFD = theFD + ".gdb/NATL_Broadband_Map/"
+        theFD =  theLocation + theST + "/" + theST + "_SBDD_" + theYear + "_"
+        theFD = theFD + theMonth + "_" + theDay + ".gdb/NATL_Broadband_Map/"        
         arcpy.AddMessage("the state is: " + theST)
-        checkOverlyRecords()
-    del theFD, theST, States, thePGDB
+        sbdd_CreateTable("wireless_block_overlay_" + theST)
+        sbdd_AppendRecords(thePGDB + "/wireless_block_overlay_" + theST)
+    del theST, States, thePGDB, theInFGDB
 except:
     arcpy.AddMessage("Something bad happened")
 
