2e3595469277b6cb38285aab0f7684e3298a9ea0
Michael Byrne <michael.byrne@fcc.gov>
2013-06-13 12:45:38 -0400

    adding support for correct lat/lon

--- Phase3/ScriptDataPrep/SBDD_Check_CAI.py
+++ Phase3/ScriptDataPrep/SBDD_Check_CAI.py
@@ -3,7 +3,7 @@
 # Created on: May 14, 2013
 # Created by: Michael Byrne
 # Federal Communications Commission
-# checks CAI for non-ascii stuff, and replaces them w ' ' 
+# checks CAI for carriage returns line feeds
 # ---------------------------------------------------------------------------
 
 # Import system modules
@@ -12,7 +12,7 @@ from arcpy import env
 import sys, string, os, math
 
 #global variables
-theOF = "C:/Users/michael.byrne/"
+theOF = "C:/Users/michael.byrne/NBM/2013_1/"
 
 States = ["AK","AL","AR","AS","AZ","CA","CO","CT"]          #1
 States = States + ["DC","DE","FL","GA","GU","HI","IA","ID"] #2
@@ -22,14 +22,13 @@ States = States + ["NE","NH","NJ","NM","NV","NY","OH","OK"] #5
 States = States + ["OR","PA","PR","RI","SC","SD","TN","TX"] #6
 States = States + ["UT","VA","VI","VT","WA","WI","WV","WY"] #7 
 
-theLocation = "C:/Users/michael.byrne/NBM/Spring2013/Data/"
+theLocation = "C:/Users/michael.byrne/NBM/2013_1/"
 theYear = "2013"
 theMonth = "04"
 theDay = "01"
 
-#Function sbdd_ProviderReport writes out the unique Provider Values Detail
-#has no argument; 
-def sbdd_exportFile (myTbl, myOutFile):
+#Function sbdd_ascii - checks and replaces non-ascii characters
+def sbdd_ascii (myTbl, myOutFile):
     #go open up and read this table
     myFile = open(myOutFile, 'a')
     rows = arcpy.UpdateCursor(myTbl)
@@ -45,25 +44,40 @@ def sbdd_exportFile (myTbl, myOutFile):
                 arcpy.AddMessage("       fixing error in sbdd_id:" + myID)
                 myStr = ''
                 for c in myVal:
-                    #arcpy.AddMessage(c.encode('ascii','replace'))
-                    myStr = myStr + c.encode('ascii','replace')
-                myStr = myStr.replace('?',' ')
-                arcpy.AddMessage("     old: " + myVal)
-                arcpy.AddMessage("     new: " + myStr)
-                row.setValue(myF, myStr)
-                rows.updateRow(row)                
-                myFile.write(theST + ": has errors on record: " + myID + "for field: " + myF + "\n") 
+#                    arcpy.AddMessage(c.encode('ascii','replace'))
+                    myStr = myStr + c.encode('ascii','replace').strip()
+                myStr = myStr.replace('?','#')
+#                arcpy.AddMessage("          old: " + myVal)
+                arcpy.AddMessage("          new: " + myStr)
+#                row.setValue(myF, myStr)
+#                rows.updateRow(row)                
+                myFile.write(theST + ": has errors on record: " + myID + " for field: " + myF + " new: " + myStr + "\n") 
     myFile.close()
     del row, rows
+
+#Function for correctly inserting the Lat/Lon
+#has argument of myTbl
+def sbdd_LatLon(myTbl):
+    rows = arcpy.UpdateCursor(myTbl)
+    for row in rows:
+        myX = row.getValue("Shape").getPart().X
+        myY = row.getValue("Shape").getPart().Y
+        arcpy.AddMessage("theValue of X is: " + str(myX) + " and the value is: " + str(row.getValue("Longitude")))
+        arcpy.AddMessage("theValue of Y is: " + str(myY) + " and the value is: " + str(row.getValue("Latitude")))
+        row.longitude = myX
+        row.latitude = myY
+        rows.updateRow(row)
+
 
 #****************************************************************************
 ##################Main Code below
 #****************************************************************************
 
-outFile = theOF + "cai_errors.txt"
-myFile = open(outFile, 'w')
-myFile.write("errors for CAI w/ non-utf-8/ascii characters" + "\n")
+
 for theST in States:
+    outFile = theOF + theST + "_cai_errors.txt"
+    myFile = open(outFile, 'w')
+    myFile.write("errors for CAI w/ non-utf-8/ascii characters" + "\n")
     theFD = theLocation + theST + "/" + theST + "_SBDD_" + theYear
     theFD = theFD + "_" + theMonth + "_" + theDay
     theFD = theFD + ".gdb/NATL_Broadband_Map/"   
@@ -77,8 +91,9 @@ for theST in States:
         theMsg = theMsg + theST + ": " + myCnt
         arcpy.AddMessage(theMsg)
         if myCnt <> '0':
-            sbdd_exportFile(theFD + "BB_Service_CAInstitutions", outFile)
-#            del myFile, outFile, theMsg, myCnt
+#            sbdd_ascii(theFD + "BB_Service_CAInstitutions", outFile)
+            sbdd_LatLon(theFD + "BB_Service_CAInstitutions")
+            del myFile, outFile, theMsg, myCnt
     else:
         theMsg = "     CAI table for " + theST
         theMsg = theMsg + " does not exist" 
@@ -86,7 +101,4 @@ for theST in States:
         del theMsg
 del theFD, theST, States #theHead, 
 del theOF, theLocation, theYear, theMonth, theDay 
-
-
-
   
